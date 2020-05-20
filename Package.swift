58e08921f712d070ff702020149904e274cbd5e6
maximkrouk <id.maximkrouk@gmail.com>
2020-05-21 01:47:34 +0300

    Graphiti dependecy changed

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["GraphQLKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/alexsteinerde/Graphiti.git", from: "0.13.0-beta"),
+        .package(url: "https://github.com/maximkrouk/Graphiti.git", .branch("mx")),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta"),
 //        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-beta"),
     ],
